ebb3ce92d5be154f54f567d14da7fc0d7b5c13c6
samkramer6 <samkramer6@vt.edu>
2023-09-05 22:25:40 -0400

    Updating Module

--- Julia Libraries/library_module.jl
+++ Julia Libraries/library_module.jl
@@ -9,10 +9,14 @@ module library
         include("load_data.jl")     # load_data() function include statement
         include("spectrogram_function.jl")
         include("five_peak_finder.jl")
+        include("create_chirp.jl")
+        include("spectrogram_function.jl")
 
     # --Export List
         export load_data
         export spectrogram_function
         export five_peak_finder
+        export spectrogram_function
+        export create_chirp
 
 end
