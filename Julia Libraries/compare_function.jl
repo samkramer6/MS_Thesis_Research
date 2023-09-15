d57f3fa353f4f8fd24ef622306d6135da6679ef6
samkramer6 <samkramer6@vt.edu>
2023-09-14 23:11:54 -0400

    Creation of Compare Function

--- Julia Libraries/compare_function.jl
+++ Julia Libraries/compare_function.jl
@@ -1,6 +1,17 @@
 #=  compare_function()
+    This is a function that is used to compare the time indeces of the peaks for a single dataset 
+    vector. It will eliminate any repeated values and will also eliminate any values that are 
+    within a certain threshold of eachother. Will return a new vector as new_ind.
 
+    Inputs:
+        Indeces::AbsVec =   This is a data vector that you would like to compare the data and eliminate all the reapeated values.
+        threshold::Float =  This is the difference threshold criteria you establish to eliminate two values and combine them to a single value. 
 
+   Ouputs:
+           new_ind::AbsVec =   This is a new vector with a compare dataset that keeps the unique values.
+
+    Sam Kramer
+    September 14th, 2023
 =#
 
 # --Include statements
@@ -10,8 +21,40 @@
 
 
 # --Function Definition
-function compare_function
+function compare_function(indeces, threshold)
 
+    # --Setup
+        new_ind = [indeces[1]];
 
+    # --Loop through entire indeces array set
+        for i = 2:eachindex(indeces)
 
+            # --Setup Loop 
+                okay = "true"
+                IIQ = indeces[i];
+
+            # --Check across previous values in new_ind
+                k = 1;
+                while k <= length(new_ind)
+
+                    # --Check difference
+                        diff_ind = abs.(new_ind[k] - IIQ);
+
+                    # --If difference is enough, continue
+                        if diff_ind >= threshold
+                            k = k+1;
+                        else
+                            okay = "false";
+                            break
+                        end
+                end
+            
+            # --Save value into new_ind
+                if okay == "true"
+                    new_ind = [new ind; IIQ];
+                end
+
+        end
+    
+    return new_ind
 end
