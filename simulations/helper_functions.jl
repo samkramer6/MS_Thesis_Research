3d7b465a9b6f9ec54e6ed6ed47e539ac0b83907a
samkramer6 <samkramer6@vt.edu>
2024-08-26 20:31:47 -0400

    Simulations work -- KS test update

--- simulations/helper_functions.jl
+++ simulations/helper_functions.jl
@@ -1,20 +1,20 @@
 #   helper_functions.jl
 
 using DSP
-using FFTW 
+using FFTW
 using Distributions
 using Plots
-  gr()
+gr()
 
 ###################################################################################################
 
 mutable struct Data
 
-    signal::Any    
+    signal::Any
     long_signal::Any
-    ref_signal::Any 
+    ref_signal::Any
     noise::Any
-    noisy_signal::Any 
+    noisy_signal::Any
     fs::Int64
     SNR::Any
 
@@ -24,7 +24,7 @@ struct Corr
 
     signal::Vector{Float64}
     noisy_signal::Vector{Float64}
-    correlation::Vector{Float64} 
+    correlation::Vector{Float64}
 
 end
 
@@ -40,23 +40,23 @@ end
 
 function populate_signal!(data::Data)
 
-    T = 1;
-    f = vec(collect(range(10, 128, data.fs))) .* 2 .* π;
-    time = vec(collect(range(0, T, data.fs)));
-    hann_window = sin.(π .* time) .^ 2;
-    signal::Vector{Float64} = vec(hann_window .* sin.(f .* time));
+    T = 1
+    f = vec(collect(range(10, 128, data.fs))) .* 2 .* π
+    time = vec(collect(range(0, T, data.fs)))
+    hann_window = sin.(π .* time) .^ 2
+    signal::Vector{Float64} = vec(hann_window .* sin.(f .* time))
 
     # --Long signal
-    signal_long::Vector{Float64} = vec( vcat( vcat(zeros(data.fs, 1), signal), zeros(data.fs, 1) ) ); 
-    
+    signal_long::Vector{Float64} = vec(vcat(vcat(zeros(data.fs, 1), signal), zeros(data.fs, 1)))
+
     # --Reference signal
-    signal_ref::Vector{Float64} = vec( vcat( vcat(signal, zeros(data.fs, 1)), zeros(data.fs, 1) ) ); 
+    signal_ref::Vector{Float64} = vec(vcat(vcat(signal, zeros(data.fs, 1)), zeros(data.fs, 1)))
 
     # --Store in data struct
-    data.signal = signal;
-    data.long_signal = signal_long;
-    data.ref_signal = signal_ref; 
-    
+    data.signal = signal
+    data.long_signal = signal_long
+    data.ref_signal = signal_ref
+
 end
 
 @inline function find_SNR!(data::Data)
@@ -66,8 +66,8 @@ end
     @fastmath signal_power = sum(data.signal .^ 2) ./ length(data.signal)
 
     # --Calculate SNR
-    @fastmath SNR::Float64 = round.(10 .* log10.(signal_power ./ noise_power), digits = 3);
-    data.SNR = SNR;
+    @fastmath SNR::Float64 = round.(10 .* log10.(signal_power ./ noise_power), digits=3)
+    data.SNR = SNR
 
 end
 
@@ -77,14 +77,14 @@ end
     noise::Vector{Float64} = α .* vec(rand(Normal(0, 0.33), 1, length(data.long_signal))) ./ β
 
     # --Create noisy signal
-    noisy_signal::Vector{Float64} = data.long_signal .+ noise; 
+    noisy_signal::Vector{Float64} = data.long_signal .+ noise
 
     # --Save to struct
-    data.noise = noise;
-    data.noisy_signal = noisy_signal;
+    data.noise = noise
+    data.noisy_signal = noisy_signal
 
     # --find SNR
-    find_SNR!(data);
+    find_SNR!(data)
 
 end
 
@@ -94,43 +94,43 @@ end
     noise::Vector{Float64} = α .* vec(rand(Normal(0, 0.33), 1, length(data.long_signal))) ./ β
 
     # --Filter noise vector to make colored noise
-        a = vec(zeros(1, 255));
-        a[1] = 1;
-        i = collect(range(2, length(a)));
-        a[i] = (0.3 .+ i .- 1) .* (a[i .- 1] ./ i);
-        filter_design = DSP.PolynomialRatio(vec(a), vec([1]));
+    a = vec(zeros(1, 255))
+    a[1] = 1
+    i = collect(range(2, length(a)))
+    a[i] = (0.3 .+ i .- 1) .* (a[i.-1] ./ i)
+    filter_design = DSP.PolynomialRatio(vec(a), vec([1]))
 
     # --Color the noise
-        noise = DSP.filt(filter_design, noise);
+    noise = DSP.filt(filter_design, noise)
 
     # --Create noisy signal
-    noisy_signal::Vector{Float64} = data.long_signal .+ noise; 
+    noisy_signal::Vector{Float64} = data.long_signal .+ noise
 
     # --Save to struct
-    data.noise = noise;
-    data.noisy_signal = noisy_signal;
+    data.noise = noise
+    data.noisy_signal = noisy_signal
 
     # --find SNR
-    find_SNR!(data);
+    find_SNR!(data)
 
 end
 
 @inline function detect_signal(data::Data, known_location::Int64, algorithm::String)
-    
+
     # --Find correlation
     if algorithm == "Cross-Correlation"
-        corr_out = xcorr(data);
+        corr_out = xcorr(data)
     elseif algorithm == "Matched Filter"
-        corr_out = matched_filter(data);
+        corr_out = matched_filter(data)
     end
-    
+
     # --Find peak location
-    max_location = findall(x -> x == 1, corr_out.correlation);
+    max_location = findall(x -> x == 1, corr_out.correlation)
     # @show abs.(max_location .- known_location) .<= 100 
-    
+
     # --Store into struct
-    detection_output = Detection(max_location[1], (abs.(max_location .- known_location) .<= 100), data.SNR); 
-        
+    detection_output = Detection(max_location[1], (abs.(max_location .- known_location) .<= 100), data.SNR)
+
     return detection_output::Detection
 end
 
@@ -138,42 +138,55 @@ end
 
     # --Populate Noise Function
     if noise == "White"
-        populate_noise!(data, α, β);
+        populate_noise!(data, α, β)
     elseif noise == "Red"
         populate_noise_colored!(data, α, β)
     end
-    
-    # --Detect location of signal
-    detection_output::Detection = detect_signal(data, data.fs, algorithm);
 
-    return detection_output::Detection 
+    # --Detect location of signal
+    detection_output::Detection = detect_signal(data, data.fs, algorithm)
+
+    return detection_output::Detection
 end
 
 @inline function find_pdf(SNRs::Vector{Float64}, Probabilities::Vector{Float64})
 
-    derivative = vec(zeros(1, length(SNRs) - 1));
+    derivative = vec(zeros(1, length(SNRs) - 1))
 
-    for i in collect(range(1, length(SNRs)-1))
-        
-        num = Probabilities[i + 1] - Probabilities[i];
-        denom = SNRs[i + 1] - SNRs[i];
-        
-        derivative[i] = num ./ denom;
-    
+    for i in collect(range(1, length(SNRs) - 1))
+
+        num = Probabilities[i+1] - Probabilities[i]
+        denom = SNRs[i+1] - SNRs[i]
+
+        derivative[i] = num ./ denom
+
     end
-    
+
     return derivative::Vector{<:Number}
+end
+
+@inline function ks_test(f1::Vector{Float64}, f2::Vector{Float64})
+
+    D_nm = maximum(abs.(f1 .- f2))
+    c_alpha = 1.358  # using an α of 0.05 for this value
+
+    n = length(f1)
+    m = length(f2)
+
+    test_statistic = c_alpha .* sqrt((n + m) ./ (n * m))
+
+    return D_nm > test_statistic
 end
 ###################################################################################################
 
 @inline function xcorr(data::Data)
-    
+
     # --Find correlation
-    corr::Vector{Float64} = vec(abs.(ifft( fft(data.noisy_signal) .* conj.(fft(data.ref_signal)) )));
-    @fastmath corr = corr ./ maximum(corr);
+    corr::Vector{Float64} = vec(abs.(ifft(fft(data.noisy_signal) .* conj.(fft(data.ref_signal)))))
+    @fastmath corr = corr ./ maximum(corr)
 
     # --Store in a new struct which is returned
-    output = Corr(data.signal, data.noisy_signal, corr);
+    output = Corr(data.signal, data.noisy_signal, corr)
 
     return output::Corr
 end
@@ -181,14 +194,14 @@ end
 @inline function matched_filter(data::Data)
 
     # --Generate filter
-    filter_design = PolynomialRatio(reverse(data.signal), vec([1]));
-    
+    filter_design = PolynomialRatio(reverse(data.signal), vec([1]))
+
     # --Filter data
-    matched_output::Vector{Float64} = vec(filt(filter_design, data.noisy_signal)); 
-    @fastmath matched_output = reverse(matched_output ./ maximum(matched_output));
-    
+    matched_output::Vector{Float64} = vec(filt(filter_design, data.noisy_signal))
+    @fastmath matched_output = reverse(matched_output ./ maximum(matched_output))
+
     # --Store in a new struct
-    corr = Corr(data.signal, data.noisy_signal, matched_output);
+    corr = Corr(data.signal, data.noisy_signal, matched_output)
 
     return corr::Corr
 end
--- simulations/main.jl
+++ simulations/main.jl
@@ -4,7 +4,7 @@ include("helper_functions.jl")
 
 using BenchmarkTools
 using Plots
-    gr()
+gr()
 using Timers
 using PlotThemes
 
@@ -14,59 +14,86 @@ function main(algorithm::String, noise::String)
 
     # --Parameters
     Timers.tic()
-    α = range(2.0, 151.0);
-    num_points = length(α); 
-    β = range(7.5, 0.1 , num_points);
-    num_sims = 2000;
-    SNRs::Vector{Float64} = vec(zeros(Bool, 1, length(α)));
-    Probabilities::Vector{Float64} = vec(zeros(Float64, 1, length(α)));
+    α = range(2.0, 151.0, 100)
+    num_points = length(α)
+    β = range(7.5, 0.1, num_points)
+    num_sims = 10000
+    SNRs::Vector{Float64} = vec(zeros(Bool, 1, length(α)))
+    Probabilities::Vector{Float64} = vec(zeros(Float64, 1, length(α)))
 
     # --Populate Signal
-    data = Data((), (), (), (), (), 1024, ());
-    populate_signal!(data);
+    data = Data((), (), (), (), (), 1024, ())
+    populate_signal!(data)
 
     # --Outer for loop (SNR loop)
     for i in collect(range(1, length(α)))
-        
-        detection_result::BitVector = BitVector(undef, num_sims);  
-        SNR_result::Vector{Float64} = vec(zeros(Float64, 1, num_sims));
 
-        for j in collect(range(1, num_sims)) 
-            
-            detection_output::Detection = roll_dice(data, α[i], β[i], algorithm, noise);
-            
-            @inbounds SNR_result[j] = detection_output.SNR;
-            @inbounds detection_result[j] = detection_output.detection[1]; 
+        detection_result::BitVector = BitVector(undef, num_sims)
+        SNR_result::Vector{Float64} = vec(zeros(Float64, 1, num_sims))
 
-        end 
-        
-        @inbounds SNRs[i] = mean(SNR_result);
-        @inbounds Probabilities[i] = mean(detection_result);         
-     
+        for j in collect(range(1, num_sims))
+
+            detection_output::Detection = roll_dice(data, α[i], β[i], algorithm, noise)
+
+            @inbounds SNR_result[j] = detection_output.SNR
+            @inbounds detection_result[j] = detection_output.detection[1]
+
+        end
+
+        @inbounds SNRs[i] = mean(SNR_result)
+        @inbounds Probabilities[i] = mean(detection_result)
+
     end
 
     # --Plot
     plot!(SNRs, Probabilities,
-         xlims = (-50, 5), 
-         ylims = (0, 1.05),
-         label = "Detection Probability Distribution",
-         guidefont = "Computer Modern",
-         xlabel = "SNR (dB)",
-         # ylabel = "Detection Probability",
-         tickfont = "Computer Modern",
-         linewidth = 1.5,
-         legendfont = "Computer Modern",
-         theme = :bright,
+        xlims=(-55, 5),
+        ylims=(0, 1.05),
+        # label = "$algorithm - $noise",
+        label="Detection Probability",
+        guidefont="Computer Modern",
+        xlabel="SNR (dB)",
+        # ylabel = "Detection Probability",
+        tickfont="Computer Modern",
+        linewidth=1.5,
+        legendfont="Computer Modern",
+        theme=:bright,
+        dpi=500,
     )
 
+    # --PDF Plotting
+    pdf_plot = find_pdf(SNRs, Probabilities)
+
+    plot!(SNRs[1:end-1], pdf_plot,
+        label="Numerical PDF",
+        linewidth=1.5,
+    )
+
+    # --PDF Fitting
+    k = 80
+    λ = 20
+    x = collect(range(1, 130, length(pdf_plot)))
+    xaxis = collect(range(-55, 5, length(x)))
+    first_derivative = 1.6 .* (λ / k) .* (((x) ./ k) .^ (λ - 1)) .* exp.(-((x) ./ k) .^ λ)
+
+    plot!(xaxis, first_derivative,
+        label="PDF Fit",
+        linewidth=1.5,
+    )
+
+    # --K-S Testing
+    ks_result = ks_test(pdf_plot, first_derivative)
+
     # --Out Statement
-    num_total_sims = num_points * num_sims;
+    num_total_sims = num_points * num_sims
     println("====== SIM DONE ======")
-    println("Algorithm Used: $algorithm") 
-    println("Noise Type Used = $noise") 
+    println("Algorithm Used: $algorithm")
+    println("Noise Type Used = $noise")
     println("sims per point = $num_sims")
-    println("Total Sims = $num_total_sims \n")
+    println("K-S Results are $ks_result")
+    println("Total Sims = $num_total_sims\n")
     Timers.toc()
+    println("\n")
 
 end
 
@@ -74,8 +101,8 @@ end
 
 plot()
 main("Cross-Correlation", "White")
-main("Matched Filter", "White")
-main("Cross-Correlation", "Red")
-main("Matched Filter", "Red")
-savefig("Detection_Output.png")
+# main("Matched Filter", "White")
+# main("Cross-Correlation", "Red")
+# main("Matched Filter", "Red")
+savefig("test_fig.png")
 
